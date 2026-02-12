995502d84a8d1f06db96071a9c7001d3a763704b
AlaricAbraham <alaricabraham1@gmail.com>
2026-02-12 03:13:41 -0600

    chore: initial project structure and database configuration

---
+++ src/main/java/com/problemsolutionsltd/ProblemSolutionsLtdApplication.java
@@ -0,0 +1,13 @@
+package com.problemsolutionsltd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProblemSolutionsLtdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProblemSolutionsLtdApplication.class, args);
+    }
+
+}
