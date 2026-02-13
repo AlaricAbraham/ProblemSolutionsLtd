8c0f28915c4d8ce810051d866feb5df5e2f62b84
AlaricAbraham <alaricabraham1@gmail.com>
2026-02-13 00:13:52 -0600

    feat: implement Category, Status, and Solution entity with JPA schema generation

---
+++ src/main/java/com/problemsolutionsltd/model/Status.java
@@ -0,0 +1,8 @@
+package com.problemsolutionsltd.model;
+
+public enum Status {
+    AVAILABLE,      // Ready to ship
+    OUT_OF_STOCK,   // We need to order more
+    DISCONTINUED,   // Old tech (e.g., "Laser v1")
+    RECALLED        // Whoops...
+}
